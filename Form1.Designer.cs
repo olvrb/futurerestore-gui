da970f33a385f8923235e4a5bc0cda2247d029ff
Oliver Boudet <oboudet1@me.com>
2018-09-07 09:36:57 +0200

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace tsschecker_gui {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.blobbutton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // blobbutton
+            // 
+            this.blobbutton.Location = new System.Drawing.Point(236, 10);
+            this.blobbutton.Name = "blobbutton";
+            this.blobbutton.Size = new System.Drawing.Size(88, 22);
+            this.blobbutton.TabIndex = 0;
+            this.blobbutton.Text = "Select blob file";
+            this.blobbutton.UseVisualStyleBackColor = true;
+            this.blobbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(330, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select signed IPSW file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(462, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Select restore-to IPSW file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(381, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 67);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(843, 411);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 490);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.blobbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button blobbutton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tsschecker_gui {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private string blobFilePath;
+        private string signedIPSWFilePath;
+        private string restoreToIPSWFilePath;
+
+        public string BlobFilePath {
+            get => blobFilePath;
+            set => blobFilePath = value;
+        }
+
+        public string SignedIPSWFilePath {
+            get => signedIPSWFilePath;
+            set => signedIPSWFilePath = value;
+        }
+
+        public string RestoreToIPSWFilePath {
+            get => restoreToIPSWFilePath;
+            set => restoreToIPSWFilePath = value;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK) {
+                this.BlobFilePath = file.FileName;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e) {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK) {
+                this.SignedIPSWFilePath = file.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK) {
+                this.RestoreToIPSWFilePath = file.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            var proc = new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = "futurerestore.exe",
+                    Arguments = "-t " + this.blobFilePath +
+                                " -i " + this.signedIPSWFilePath + 
+                                " " + this.restoreToIPSWFilePath,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            proc.Start();
+            while (!proc.StandardOutput.EndOfStream) {
+                richTextBox1.Text += proc.StandardOutput.ReadLine();
+            }
+        }
+    }
+}
