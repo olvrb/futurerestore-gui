5ff37e83da555a46adbe4498027906add7214df2
rt50631 <37247093+rt50631@users.noreply.github.com>
2018-09-08 15:54:23 +0200

    UI redesign, confirmation boxes, support for no-baseband, some fixes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,11 +28,16 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // blobbutton
             // 
-            this.blobbutton.Location = new System.Drawing.Point(220, 8);
+            this.blobbutton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.blobbutton.Location = new System.Drawing.Point(16, 12);
             this.blobbutton.Name = "blobbutton";
             this.blobbutton.Size = new System.Drawing.Size(132, 24);
             this.blobbutton.TabIndex = 0;
@@ -42,7 +47,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(358, 8);
+            this.button1.Location = new System.Drawing.Point(154, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(126, 24);
             this.button1.TabIndex = 1;
@@ -52,7 +57,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(490, 8);
+            this.button2.Location = new System.Drawing.Point(286, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(143, 24);
             this.button2.TabIndex = 2;
@@ -62,36 +67,90 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(384, 38);
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button3.Enabled = false;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(659, 22);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
             this.button3.Text = "Start";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // richTextBox1
             // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.richTextBox1.Enabled = false;
-            this.richTextBox1.Location = new System.Drawing.Point(12, 67);
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 61);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(843, 411);
+            this.richTextBox1.Size = new System.Drawing.Size(843, 417);
             this.richTextBox1.TabIndex = 4;
             this.richTextBox1.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI Light", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(16, 33);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 22);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Path";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI Light", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(154, 33);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(126, 22);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Path";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Segoe UI Light", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(286, 33);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(143, 22);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Path";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(444, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.Items.AddRange(new object[] {" ",
+                        "With baseband",
+                        "Without baseband"});
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(867, 490);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.blobbutton);
+            this.Font = new System.Drawing.Font("Segoe UI Light", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.Name = "Form1";
             this.Text = "Futurerestore GUI";
-            this.ShowIcon = false;
             this.ResumeLayout(false);
 
         }
@@ -103,5 +162,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -28,43 +28,142 @@ namespace tsschecker_gui {
         }
 
         private void Button1_Click(object sender, EventArgs e) {
-            OpenFileDialog file = new OpenFileDialog();
+            OpenFileDialog file = new OpenFileDialog
+            {
+                Filter = "Blob file (*.SHSH2)|*.SHSH2|All files (*.*)|*.*"
+            };
             if (file.ShowDialog() == DialogResult.OK) {
                 this.BlobFilePath = file.FileName;
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBox.Show("File added.", "File added!", buttons, MessageBoxIcon.Information);
+            }
+            if (this.blobFilePath != null && this.signedIPSWFilePath != null && this.restoreToIPSWFilePath != null)
+            {
+                this.button3.Enabled = true;
             }
         }
 
         private void Button1_Click_1(object sender, EventArgs e) {
-            OpenFileDialog file = new OpenFileDialog();
+            OpenFileDialog file = new OpenFileDialog
+            {
+                Filter = "IPSW file (*.ipsw)|*.ipsw|All files (*.*)|*.*"
+            };
             if (file.ShowDialog() == DialogResult.OK) {
                 this.SignedIPSWFilePath = file.FileName;
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBox.Show("File added.", "File added!", buttons, MessageBoxIcon.Information);
+            }
+            if (this.blobFilePath != null && this.signedIPSWFilePath != null && this.restoreToIPSWFilePath != null)
+            {
+                this.button3.Enabled = true;
             }
         }
 
         private void Button2_Click(object sender, EventArgs e) {
-            OpenFileDialog file = new OpenFileDialog();
+            OpenFileDialog file = new OpenFileDialog
+            {
+                Filter = "IPSW file (*.ipsw)|*.ipsw|All files (*.*)|*.*"
+            };
             if (file.ShowDialog() == DialogResult.OK) {
                 this.RestoreToIPSWFilePath = file.FileName;
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBox.Show("File added.", "File added!", buttons, MessageBoxIcon.Information);
+            }
+            if (this.blobFilePath != null && this.signedIPSWFilePath != null && this.restoreToIPSWFilePath != null)
+            {
+                this.button3.Enabled = true;
             }
         }
 
         private void Button3_Click(object sender, EventArgs e) {
-            Process proc = new Process {
-                StartInfo = new ProcessStartInfo {
-                    FileName = "futurerestore.exe",
-                    Arguments = "-t " + this.blobFilePath +
-                                " -i " + this.signedIPSWFilePath +
-                                "--latest-sep" +
-                                "--latest-baseband" + 
-                                " " + this.restoreToIPSWFilePath,
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true
+            if (comboBox1.SelectedItem.ToString() == "With baseband")
+            {
+                Process proc = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "futurerestore.exe",
+                        Arguments = "-t " + this.blobFilePath +
+                    " -i " + this.signedIPSWFilePath +
+                    "--latest-sep" +
+                    "--latest-baseband" +
+                    " " + this.restoreToIPSWFilePath,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+                proc.Start();
+                while (!proc.StandardOutput.EndOfStream)
+                {
+                    richTextBox1.Text += proc.StandardOutput.ReadLine() + "\n";
                 }
-            };
-            proc.Start();
-            while (!proc.StandardOutput.EndOfStream) {
-                richTextBox1.Text += proc.StandardOutput.ReadLine() + "\n";
+            }
+            if (comboBox1.SelectedItem == " " || comboBox1.SelectedItem == null || comboBox1.SelectedItem == "")
+            {
+                MessageBox.Show("Select a device type!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (comboBox1.SelectedItem.ToString() == "Without baseband")
+            {
+                Process proc = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "futurerestore.exe",
+                        Arguments = "-t " + this.blobFilePath +
+                    " -i " + this.signedIPSWFilePath +
+                    "--latest-sep" +
+                    "--no-baseband" +
+                    " " + this.restoreToIPSWFilePath,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+                proc.Start();
+                while (!proc.StandardOutput.EndOfStream)
+                {
+                    richTextBox1.Text += proc.StandardOutput.ReadLine() + "\n";
+                }
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            if (this.blobFilePath == null)
+            {
+                MessageBox.Show("Add a file!", "Error", buttons, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show(this.blobFilePath, "Current path", buttons, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            if (this.signedIPSWFilePath == null)
+            {
+                MessageBox.Show("Add a file!", "Error", buttons, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show(this.signedIPSWFilePath, "Current path", buttons, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            if (this.restoreToIPSWFilePath == null)
+            {
+                MessageBox.Show("Add a file!", "Error", buttons, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show(this.restoreToIPSWFilePath, "Current path", buttons, MessageBoxIcon.Information);
             }
         }
     }
